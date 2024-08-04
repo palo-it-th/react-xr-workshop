5dcd061b0ab4c227be5e882af2365c3a21ce5575
akkaponsomjai <asomjai@palo-it.com>
2024-08-04 21:10:43 +0700

    chore: Add THREE.js dependencies and configure lighting and scene in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { createXRStore, XR } from '@react-three/xr';
 import SimpleMeshScene from './scene/SimpleMeshScene';
 import { ErrorBoundary } from 'react-error-boundary';
+import * as THREE from 'three'; // Add this line
 
 const store = createXRStore({ frameRate: 'high' });
 
@@ -12,7 +13,11 @@ function App() {
       <ErrorBoundary FallbackComponent={() => <div>Something went wrong</div>}>
         <Canvas>
           <XR store={store}>
-            <ambientLight />
+            <color attach="background" args={['#111']} />
+            <ambientLight intensity={2} />
+            <pointLight position={[20, 10, -10]} intensity={2} />
+            <primitive object={new THREE.AxesHelper(2)} />
+            <primitive object={new THREE.GridHelper(20, 20)} />
             <SimpleMeshScene />
           </XR>
         </Canvas>
