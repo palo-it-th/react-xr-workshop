6682376c5ba873e3ebd588559e4a61c3c2777d33
akkaponsomjai <asomjai@palo-it.com>
2024-10-09 18:26:41 +0700

    Refactor Atom component to use BatchedRenderer for better performance

--- workshop/src/VFX/components/Atom.tsx
+++ workshop/src/VFX/components/Atom.tsx
@@ -8,36 +8,42 @@ interface AtomProps {
 }
 
 export default function Atom({ position }: AtomProps) {
-  const ref = useRef<Mesh>(null);
-  const [batchRenderer, setBatchRenderer] = useState(new BatchedRenderer());
+  const [batchRenderer] = useState(new BatchedRenderer());
 
   useFrame((_, delta) => {
-    if (ref.current) {
-      ref.current.rotation.x = ref.current.rotation.y += 0.01;
-    }
+    // Keep the batch renderer updated every frame
     batchRenderer.update(delta);
   });
+
   const { scene } = useThree();
 
   useEffect(() => {
     const loader = new QuarksLoader();
 
+    // Ignore the cross-origin restriction
     loader.setCrossOrigin('');
+
+    // Load the atom resource from https://quarks.art/ 
+    // then put it to public folder
     loader.load(
       '/particles/atom.json',
       (obj) => {
         obj.traverse((child) => {
+          // Add the particle emitter to the batch renderer if object node is ParticleEmitter
           if (child.type === 'ParticleEmitter') {
             batchRenderer.addSystem((child as any).system);
           }
         });
+        
+        // Set the scale and position of the atom in the scene
         obj.scale.set(0.1, 0.1, 0.1);
         obj.position.copy(position);
+
+        // Add the atom to the scene
         scene.add(obj);
-      },
-      () => {},
-      () => {},
+      }
     );
+    // Add the batch renderer to the scene
     scene.add(batchRenderer);
   }, []);
 
