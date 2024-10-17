1030a51202b6726d02c7f9a207e411d4e933d22e
Nat-Manatsawi <msutlaeo@palo-it.com>
2024-10-17 14:58:41 +0700

    chore: clean up code format

--- workshop/src/UI/components/ExampleHTML.tsx
+++ workshop/src/UI/components/ExampleHTML.tsx
@@ -6,22 +6,29 @@ import { Card } from '@react-three/uikit-default';
 import { DoubleSide, MeshBasicMaterial, MeshPhongMaterial } from 'three';
 
 export default function HTMLExample() {
-    return (
-        <group>
-            <Card width={560} height={315} borderRadius={32} padding={32} gap={8} flexDirection="column">
-                <group  scale={[0.37, 0.37, 0.37]}>
-                    <Html transform  >
-                        <iframe
-                            width="560"
-                            height="315"
-                            src="https://www.youtube.com/embed/ZHgyQGoeaB0?si=VRxS3J-TvLmE4cSI"
-                            title="YouTube video player"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                    </Html>
-                </group>
-            </Card>
+  return (
+    <group>
+      <Card
+        width={560}
+        height={315}
+        borderRadius={32}
+        padding={32}
+        gap={8}
+        flexDirection="column"
+      >
+        <group scale={[0.37, 0.37, 0.37]}>
+          <Html transform>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/ZHgyQGoeaB0?si=VRxS3J-TvLmE4cSI"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </Html>
         </group>
-    );
+      </Card>
+    </group>
+  );
 }
